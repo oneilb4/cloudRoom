8319a44448f4d909d0fad8eee814ed8a18e78910
oneilb4 <boneil@saic.edu>
2021-03-11 17:53:32 -0600

    Fixed background image, deleted extra gltf files, made edits to scene.

--- js/main.js
+++ js/main.js
@@ -13,25 +13,33 @@ function setup(){
   camera.position.z = 3
   scene.add(camera)
 
-//hemisphere light
+//lights
   const light = new THREE.HemisphereLight(0x8F6BFF, 0x8F6BFF, 3)
   scene.add(light)
+
+  const sphere = new THREE.SphereGeometry(0.2, 16, 8)
+  const mat = new THREE.MeshBasicMaterial({ color: 0xFFCF67})
+  const lightSphere = new THREE.Mesh(sphere, mat)
+  light1 = new THREE.PointLight(0xBF4FFF, 1, 50)
+  light1.add(lightSphere)
+  scene.add(light1)
+  light1.position.set(0, 10, 0)
 
 //ground
   const geometry = new THREE.PlaneGeometry(50, 50, 50, 50)
   const material = new THREE.MeshBasicMaterial({
     side: THREE.DoubleSide,
-    color: 0x8F6BFF,
+    color: 0x4FDCFF,
     //wireframe: true,
   })
   const plane = new THREE.Mesh(geometry, material)
   scene.add(plane)
   plane.rotation.x = Math.PI / 2
+  plane.position.y = -3
 
 //cloud arrays
   const loader = new THREE.GLTFLoader().setPath('models/')
   loader.load('cloud-array.gltf', (gltf) => {
-    console.log(gltf)
     const cloudarray = gltf.scene.children[0]
     cloudarray.position.x = 0
     cloudarray.position.y = 6
@@ -40,7 +48,6 @@ function setup(){
 
   const loader2 = new THREE.GLTFLoader().setPath('models/')
   loader.load('cloud-array.gltf', (gltf) => {
-    console.log(gltf)
     const cloudarray2 = gltf.scene.children[0]
     cloudarray2.position.z = 18
     cloudarray2.position.x = 6
@@ -50,7 +57,6 @@ function setup(){
 
   const loader3 = new THREE.GLTFLoader().setPath('models/')
   loader.load('cloud-array.gltf', (gltf) => {
-    console.log(gltf)
     const cloudarray3 = gltf.scene.children[0]
     cloudarray3.position.x = 12
     cloudarray3.position.y = 6
@@ -59,7 +65,6 @@ function setup(){
 
   const loader4 = new THREE.GLTFLoader().setPath('models/')
   loader.load('cloud-array.gltf', (gltf) => {
-    console.log(gltf)
     const cloudarray4 = gltf.scene.children[0]
     cloudarray4.position.x = -12
     cloudarray4.position.y = 6
@@ -68,7 +73,6 @@ function setup(){
 
   const loader5 = new THREE.GLTFLoader().setPath('models/')
   loader.load('cloud-array.gltf', (gltf) => {
-    console.log(gltf)
     const cloudarray5 = gltf.scene.children[0]
     cloudarray5.position.z = 18
     cloudarray5.position.x = -6
@@ -78,7 +82,6 @@ function setup(){
 
   const loader6 = new THREE.GLTFLoader().setPath('models/')
   loader.load('cloud-array.gltf', (gltf) => {
-    console.log(gltf)
     const cloudarray6 = gltf.scene.children[0]
     cloudarray6.position.z = -14
     cloudarray6.position.x = -6
@@ -88,7 +91,6 @@ function setup(){
 
   const loader7 = new THREE.GLTFLoader().setPath('models/')
   loader.load('cloud-array.gltf', (gltf) => {
-    console.log(gltf)
     const cloudarray7 = gltf.scene.children[0]
     cloudarray7.position.z = -14
     cloudarray7.position.x = 6
@@ -96,15 +98,24 @@ function setup(){
     scene.add(cloudarray7)
   })
 
+  const loader8 = new THREE.GLTFLoader().setPath('models/')
+  loader.load('torus-links.gltf', (gltf) => {
+    const torusLinks = gltf.scene
+    torusLinks.position.y = -45
+    torusLinks.position.z = -8
+    torusLinks.scale.set(14, 14, 14)
+    scene.add(torusLinks)
+  })
+
   renderer = new THREE.WebGLRenderer({
-    //alpha: true,
+    alpha: true,
   })
   //renderer.autoClearColor = 0xF7BDFF
   renderer.setSize(innerWidth, innerHeight)
   document.body.appendChild(renderer.domElement)
 
   controls = new THREE.OrbitControls(camera, renderer.domElement)
-  controls.target.set(0, 0, 0)
+  //controls.target.set(0, 0, 0)
   controls.minDistance = 5;
   controls.maxDistance = 8;
   controls.update()
